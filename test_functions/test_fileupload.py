73723cd5fdc981ae98e1439cd13c5bd25b77c48b
moorthyei <moorthyei@gmail.com>
2024-07-23 16:58:42 +0530

    changes for file upload test data

--- test_functions/test_fileupload.py
+++ test_functions/test_fileupload.py
@@ -1,9 +1,18 @@
 import time
 from pagefunctions.FileUpload import FileUpload
+import utilities.customlogger as lg
 import pytest
+import os
 
 
 class TestFileUpload:
+    log = lg.custom_logger()
+
+    def get_filepath(self):
+        file_dir = os.path.abspath(os.getcwd())
+        self.log.info(file_dir)
+        file_path = f"{file_dir}/test_data/uploadfile.txt"
+        return file_path
 
     def test_upload_file(self,setup_browser):
         try:
@@ -11,7 +20,7 @@ class TestFileUpload:
             self.fu = FileUpload(self.driver)
             self.fu.go_to_file_upload_page()
             # For File upload, send the filepath directly in to File upload web element without clicking on it
-            self.fu.upload_file("/Users/moorthys/Documents/Priya/resume/Resume-Mohana-Priya-Nagarajan.pdf")
+            self.fu.upload_file(self.get_filepath())
             time.sleep(2)
             self.fu.click_upload_button()
             actual_response = self.fu.get_file_upload_response_text()
